89a57a9976465e4671e3d268eed73d96a4187546
bata64 <bata64@gmail.com>
2019-11-01 04:31:32 +0900

    00,01を追加

---
+++ 00.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+text = 'stressed'
+reverse_text = text[::-1]
+
+print (reverse_text)
---
+++ 01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+text = 'パタトクカシーー'
+word_one = text[0]
+word_three = text[2]
+word_five = text[4]
+word_seven = text[6]
+
+word_convine = word_one + word_three + word_five + word_seven
+
+print (word_convine)
