f2f0f4e12f0b3e60a600cd0289cd6828f229527f
bata64 <bata64@gmail.com>
2019-11-08 16:30:12 +0900

    変数の綴り間違いを修正

--- 03.py
+++ 03.py
@@ -3,14 +3,14 @@
 import re
 
 text = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-count_lsit = list()
+count_list = list()
 
 text_mod = re.sub('\.',"",text)
 list1 = re.split(' |, ', text_mod)
 
 for word in list1:
     count = len(word)
-    count_lsit.append(count)
+    count_list.append(count)
 
-print(count_lsit)
+print(count_list)
 
