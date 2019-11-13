38f6d4fb8e34087aa50a8d7fe39292bd33078154
bata64 <bata64@gmail.com>
2019-11-13 18:08:28 +0900

    05作成中

---
+++ 05.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    text = 'I am an NLPer'
+    result_word = &ngram( str1, 'word', 2 )
+    result_char = &ngram( str1, 'char', 2 )
+
+def ngram(text, type, num):
+    print(test)
+
+if __name__ == '__main__':
+    main()
