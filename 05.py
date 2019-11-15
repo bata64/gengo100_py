537336e38b12f5e050bcbe36d171a3441d2e7473
bata64 <bata64@gmail.com>
2019-11-15 18:05:23 +0900

    05途中

--- 05.py
+++ 05.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
+import re
+
 def main():
-    text = 'I am an NLPer'
-    result_word = &ngram( str1, 'word', 2 )
-    result_char = &ngram( str1, 'char', 2 )
+    str1 = 'I am an NLPer'
+    result_word = ngram( str1, 'word', 2 )
+    result_char = ngram( str1, 'char', 2 )
 
 def ngram(text, type, num):
-    print(test)
+    if re.compile(r'^word$').search( type ):
+        list1 = re.split(' |, ', text)
+    elif re.compile(r'^char$').search( type ):
+        text_mod = re.sub(' ','',text)
+        list1 = list(text_mod)
+    print(list1)
 
 if __name__ == '__main__':
     main()
