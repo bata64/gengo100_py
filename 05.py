31b917ae32b202d49fa887386e0dfc6c29cd1fbf
bata64 <bata64@gmail.com>
2019-12-19 17:17:00 +0900

    05途中

--- 05.py
+++ 05.py
@@ -6,6 +6,7 @@ def main():
     str1 = 'I am an NLPer'
     result_word = ngram( str1, 'word', 2 )
     result_char = ngram( str1, 'char', 2 )
+    print(result_word,result_char)
 
 def ngram(text, type, num):
     if re.compile(r'^word$').search( type ):
@@ -13,7 +14,15 @@ def ngram(text, type, num):
     elif re.compile(r'^char$').search( type ):
         text_mod = re.sub(' ','',text)
         list1 = list(text_mod)
-    print(list1)
+    
+    if num == 1:
+        return list1
+    else:
+        for i in range(len(list1)):
+            data_ngram = list1[ i ]
+            for j in range( num ):
+        return list1
+        
 
 if __name__ == '__main__':
     main()
